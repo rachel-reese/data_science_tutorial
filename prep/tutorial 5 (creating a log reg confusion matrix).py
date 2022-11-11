cfdf87e56a15dce16e82c2492b090ee34637b2d4
rachel-reese <98109102+rachel-reese@users.noreply.github.com>
2022-11-11 14:22:10 -0500

    Update tutorial 5 (creating a log reg confusion matrix).py

--- prep/tutorial 5 (creating a log reg confusion matrix).py
+++ prep/tutorial 5 (creating a log reg confusion matrix).py
@@ -1,40 +1,9 @@
-def orgs_to_bots(input_path):
-    df = pd.read_csv(input_path)
-    df = df.replace({'label': 2}, {'label': 1})
-    return df
-
-
-def find_zscore(df):
-    columns = list(df.columns)
-    new_df = pd.DataFrame()
-    for col in columns:
-        new_col = (df[col] - df[col].mean()) / df[col].std(ddof=0)
-        new_df[col] = new_col
-    return new_df
-
-
-def remove_outliers(zscore, input_df, output_path=None):
-    human_df = input_df[input_df['label'] == 0]
-    bot_df = input_df[input_df['label'] == 1]
-
-    human_df = human_df[np.abs(find_zscore(human_df.loc[:, "astroturf":"self-declared"]) < zscore).all(axis=1)]
-    bot_df = bot_df[np.abs(find_zscore(bot_df.loc[:, "astroturf":"self-declared"]) < zscore).all(axis=1)]
-
-    output_df = pd.concat([human_df, bot_df])
-    output_df = output_df.loc[:, "astroturf":]
-
-    if output_path is None:
-        return output_df
-    else:
-        output_df.to_csv(output_path)
-        return output_df
-
-
-def logistic_regression(input_df, print_result=False):
+def confusion_matrix(lang, input_df, show=False):
     x = input_df.drop(["spammer", "label"], axis=1).values
     if "id" in input_df.columns:
         x = input_df.drop(["id", "CAP", "spammer", "label"], axis=1).values
     y = input_df["label"].values
+
     x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.30, random_state=100)
 
     x_train_scaled = preprocessing.scale(x_train)
@@ -42,32 +11,30 @@ def logistic_regression(input_df, print_result=False):
 
     model = LogisticRegression()
     model.fit(x_train_scaled, y_train)
-    result = model.score(x_test_scaled, y_test)
 
-    pickle.dump(model, open(f'log_reg_eng.dat', 'wb'))
+    title_options = [(f"Confusion Matrix, without normalization\n({lang})", None),
+                     (f"Normalization: true\n({lang})", "true"),
+                     (f"Normalization: pred\n({lang})", "pred"),
+                     (f"Normalization: all\n({lang})", "all")]
 
-    if print_result:
-        print("Accuracy: %.2f%%" % (result * 100.0))
-    return result * 100
+    for title, normalize in title_options:
+        disp = ConfusionMatrixDisplay.from_estimator(model, x_test_scaled, y_test, display_labels=["bot", "human"],
+                                                     cmap=plt.cm.Blues, normalize=normalize)
+        disp.ax_.set_title(title)
+        plt.savefig(fr'C:\Users\Rachel\Documents\Twitter Data\{normalize}_{lang}_test')
 
-
-def best_zscore(zscore, lang, input_path, show=False):
-    input_df = orgs_to_bots(input_path)
-    zscores = []
-    accuracies = []
-
-    while zscore >= 0:
-        zscores.append(zscore)
-        df = remove_outliers(zscore, input_df)
-        result = logistic_regression(df)
-        accuracies.append(float("{:.2f}".format(result)))
-        zscore -= 0.1
-
-    plt.plot(zscores, accuracies)
-    plt.xlabel('zscore')
-    plt.ylabel('accuracy (%)')
-    plt.suptitle(f"Zscore and Accuracy ({lang})")
-
-    plt.savefig(fr'C:\Users\Rachel\Documents\Twitter Data\zscore_{lang}_test')
     if show:
         plt.show()
+
+
+def main(zscore, lang, input_path, output_path, show_matrix=False, print_result=False,):
+    df = orgs_to_bots(input_path)
+    df_trimmed = remove_outliers(zscore, df, output_path)
+    logistic_regression(df_trimmed, print_result)
+    confusion_matrix(lang, df_trimmed, show_matrix)
+
+
+best_zscore(3, "en", input_path_en, True)
+best_zscore(3, "un", input_path_un, True)
+main(1.7, "en", input_path_en, en_path_trimmed)
+main(1.4, "un", input_path_un, un_path_trimmed, True, True)
